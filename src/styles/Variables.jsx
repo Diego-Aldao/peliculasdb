e3cfad5b4250c884c65d9c5b2b1012f56050c0aa
Diego Aldao <diego_ind@live.com.ar>
2022-10-19 14:24:44 -0300

    agregado el selector root

--- src/styles/Variables.jsx
+++ src/styles/Variables.jsx
@@ -4,24 +4,26 @@ const Variables = css`
   @import url("https://fonts.googleapis.com/css2?family=Anton&display=swap");
   @import url("https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;600&display=swap");
 
-  --color-1: linear-gradient(
-    90deg,
-    hsla(265, 52%, 88%, 1) 0%,
-    hsla(348, 56%, 89%, 1) 25%,
-    hsla(14, 51%, 89%, 1) 50%,
-    hsla(58, 34%, 86%, 1) 75%,
-    hsla(209, 71%, 93%, 1) 100%
-  );
-  --color-2: linear-gradient(
-    90deg,
-    hsla(145, 54%, 89%, 1) 0%,
-    hsla(196, 87%, 79%, 1) 33%,
-    hsla(238, 72%, 86%, 1) 66%,
-    hsla(235, 63%, 84%, 1) 100%
-  );
+  :root {
+    --color-1: linear-gradient(
+      90deg,
+      hsla(265, 52%, 88%, 1) 0%,
+      hsla(348, 56%, 89%, 1) 25%,
+      hsla(14, 51%, 89%, 1) 50%,
+      hsla(58, 34%, 86%, 1) 75%,
+      hsla(209, 71%, 93%, 1) 100%
+    );
+    --color-2: linear-gradient(
+      90deg,
+      hsla(145, 54%, 89%, 1) 0%,
+      hsla(196, 87%, 79%, 1) 33%,
+      hsla(238, 72%, 86%, 1) 66%,
+      hsla(235, 63%, 84%, 1) 100%
+    );
 
-  --fuente-principal: "Anton", sans-serif;
-  --fuente-secundaria: "Work Sans", sans-serif;
+    --fuente-principal: "Anton", sans-serif;
+    --fuente-secundaria: "Work Sans", sans-serif;
+  }
 `;
 
 export default Variables;
