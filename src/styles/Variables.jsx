a6a7e958ff032534f2a7b10db7c1420be7d9e9a1
Diego Aldao <diego_ind@live.com.ar>
2022-10-20 18:10:00 -0300

    removidos los imports de las fuentes, cambiados los colores de hsla a hex, y cambiadas las tipografias

--- src/styles/Variables.jsx
+++ src/styles/Variables.jsx
@@ -1,28 +1,20 @@
 import { css } from "styled-components";
+import "../styles/styles.css";
 
 const Variables = css`
-  @import url("https://fonts.googleapis.com/css2?family=Anton&display=swap");
-  @import url("https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;600&display=swap");
-
   :root {
     --color-1: linear-gradient(
       90deg,
-      hsla(265, 52%, 88%, 1) 0%,
-      hsla(348, 56%, 89%, 1) 25%,
-      hsla(14, 51%, 89%, 1) 50%,
-      hsla(58, 34%, 86%, 1) 75%,
-      hsla(209, 71%, 93%, 1) 100%
+      #ded0f0 0%,
+      #f3d3da 25%,
+      #f1dbd5 50%,
+      #e7e7cf 75%,
+      #e0eefa 100%
     );
-    --color-2: linear-gradient(
-      90deg,
-      hsla(145, 54%, 89%, 1) 0%,
-      hsla(196, 87%, 79%, 1) 33%,
-      hsla(238, 72%, 86%, 1) 66%,
-      hsla(235, 63%, 84%, 1) 100%
-    );
+    --color-2: linear-gradient(90deg, #80d2f0 0%, #b2b3f1 66%, #a8aff1 100%);
 
-    --fuente-principal: "Anton", sans-serif;
-    --fuente-secundaria: "Work Sans", sans-serif;
+    --fuente-principal: "Urbanist", sans-serif;
+    --fuente-secundaria: "Josefin Sans", sans-serif;
   }
 `;
 
